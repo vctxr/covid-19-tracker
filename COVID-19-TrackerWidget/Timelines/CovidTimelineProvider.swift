5346adc4cf64eb954a77df5300740ddde4760d8d
vctxr <victorsamuelcuaca131@gmail.com>
2022-05-01 16:17:44 +0700

    Make widget placeholder

--- COVID-19-TrackerWidget/Timelines/CovidTimelineProvider.swift
+++ COVID-19-TrackerWidget/Timelines/CovidTimelineProvider.swift
@@ -6,55 +6,18 @@
 //
 
 import WidgetKit
-import Networking
 
 struct CovidTimelineProvider: TimelineProvider {
-    private let networkService = NetworkingService()
-    
     func placeholder(in context: Context) -> CovidEntry {
         .placeholder
     }
 
     func getSnapshot(in context: Context, completion: @escaping (CovidEntry) -> ()) {
-        fetchTimeseriesEntry { result in
-            switch result {
-            case .success(let entry):
-                completion(entry)
-            case .failure:
-                completion(.placeholder)
-            }
-        }
+        completion(.placeholder)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<CovidEntry>) -> ()) {
-        fetchTimeseriesEntry { result in
-            switch result {
-            case .success(let entry):
-                let tomorrow = Calendar.current.date(byAdding: DateComponents(day: 1), to: Date())!
-                let tomorrowMidnight = Calendar.current.startOfDay(for: tomorrow)
-                let timeline = Timeline(entries: [entry], policy: .after(tomorrowMidnight))
-                completion(timeline)
-            case .failure:
-                let twoMinutes = Calendar.current.date(byAdding: .minute, value: 2, to: Date())!
-                let timeline = Timeline(entries: [CovidEntry.placeholder], policy: .after(twoMinutes))
-                completion(timeline)
-            }
-        }
-    }
-    
-    private func fetchTimeseriesEntry(completion: @escaping (Result<CovidEntry, NetworkError>) -> Void) {
-        let publisher = networkService.request(with: CountryListEndpoint.covidTimeseries.urlRequest)
-            .map(\CovidTimeseriesResponse.countries)
-            .eraseToAnyPublisher()
-        
-        Task {
-            do {
-                let timeseriesData = try await publisher.async()
-                let entry = CovidEntry(date: Date(), timeseriesData: timeseriesData)
-                completion(.success(entry))
-            } catch {
-                completion(.failure(NetworkError(error: error)))
-            }
-        }
+        let timeline = Timeline(entries: [CovidEntry.placeholder], policy: .never)
+        completion(timeline)
     }
 }
